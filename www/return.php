08794b5c0896cf4ca8e6c3c1709dc316bbb39934
Richard Baker <rwbaker@ge.com>
2018-07-08 18:39:30 -0500

    Updated JS paths to local copies

--- www/return.php
+++ www/return.php
@@ -100,8 +100,8 @@
   <body>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="bootstrap-4/js/bootstrap.bundle.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="bootstrap-4/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="bootstrap-4/js/bootstrap.bundle.min.js"></script>
 
     <div class="container">
       <div class="row">
